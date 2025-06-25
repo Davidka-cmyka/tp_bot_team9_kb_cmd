5f682901cf8c176a8174424f4ec1302dce622ced
Davidka-cmyka <daleksian@gmail.com>
2025-06-25 21:37:15 +0300

    добавили новые команды

--- handlers/bot_commands.py
+++ handlers/bot_commands.py
@@ -2,8 +2,13 @@ from aiogram.types import BotCommand, BotCommandScopeDefault
 
 async def set_commands(bot):
     commands = [
-        BotCommand(command='start', description='Старт'),
-        BotCommand(command='status', description='Статус'),
+        BotCommand(command='start', description='Приветствие и справка '),
+        BotCommand(command='status', description='Информация о пользователе'),
         BotCommand(command='help', description='Справка'),
+        BotCommand(command='today', description='Текущие курсы ключевых валют'),
+        BotCommand(command='subscribe', description='Включение ежедневную рассылку'),
+        BotCommand(command='unsubscribe', description='Выключение рассылки'),
+
+
                 ]
     await bot.set_my_commands(commands, BotCommandScopeDefault())
